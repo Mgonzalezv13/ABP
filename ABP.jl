885dfc5680d69dfc3ac9bbd598f0ac677a062df3
Mayron González <mgonzalezv2017@udec.cl>
2023-05-06 16:46:02 -0400

    Se cambiaron cosas, no me convence

--- ABP.jl
+++ ABP.jl
@@ -30,47 +30,20 @@ for i in 1:n_trayectorias-1
         y[i+1] = y[i] .+ v*sin(φ[i])*dt .+ sqrt(2*Dt*Dr*dt)*randn()
         φ[i+1] = φ[i] .+ Ω*dt .+ sqrt(2*Dt*Dr*dt)*randn()
         Δt[i+1]  =+ Δt[i] .+ dt
+        msd[i+1] =(x[i] .- x[1]).^2 + (y[i] .- y[1]).^2
 end
 
 
 
-# create a matrix and header
-
-#header = ["Particula_1", "Particula_2", "Particula_3", "Particula_4", "Particula_5", "Particula_6", "Particula_7", "Particula_8", "Particula_9", "Particula_10"]
-
-# convert the matrix to a DataFrame
-#df = DataFrame(x, header)
-
-# define the path and filename for the CSV file
-#filename = "posicion_x.csv"
-
-# write the DataFrame to the CSV file
-#CSV.write(filename, df)
-
-#header = ["Particula_1", "Particula_2", "Particula_3", "Particula_4", "Particula_5", "Particula_6", "Particula_7", "Particula_8", "Particula_9", "Particula_10"]
-
-# convert the matrix to a DataFrame
-#df = DataFrame(y, header)
-
-# define the path and filename for the CSV file
-#filename = "posicion_y.csv"
-
-# write the DataFrame to the CSV file
-#CSV.write(filename, df)
 
 
 
 
 
-plot(x,y)
-
-for i in 1:n_trayectorias-1
-    msd[i+1] = mean((x[i] .- x[1]).^2 + (y[i] .- y[1]).^2)
-end
 
 
 
-plot(Δt,msd, yaxis=log)
-savefig("emesdi.png")
+
+
 
 
