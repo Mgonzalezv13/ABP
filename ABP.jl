0af12a515e5b5842228e9aee7e6bb42d5faf0bb1
Mayron González <mgonzalezv2017@udec.cl>
2023-05-10 11:58:01 -0400

    Ahora se intenta calcular el msd a partir de una matriz de posiciones en x e y, aun no es funcional.

--- ABP.jl
+++ ABP.jl
@@ -1,53 +1,35 @@
-using Distributions, LinearAlgebra, GLM, DataFrames, Printf, CSV
-using Plots, Plots.PlotMeasures, StatsPlots
+using Distributions
+using Plots
 import Random
 
-
-#aca se define el numero de componentes en la direccion x e y
 n_trayectorias = 1000000
 n_particulas = 10
 
-#Aca se definen las constantes 
 L  = 40
-v  = 10.0
-Dt = 0.08
-Dr = 0.16
-Ω  = 0.0
+v  = fill(10.0, n_trayectorias)
+Dt = fill(0.08, n_trayectorias)
+Dr = fill(0.16, n_trayectorias)
+Ω  = zeros(n_trayectorias)
 dt = 10e-3
 
-#Aca se definen vectores "vacios
-x   = zeros(n_trayectorias)
+x   = zeros(n_trayectorias, n_particulas)
 y   = similar(x)
 φ   = similar(x)
-Δt  = zeros(n_trayectorias)
-msd = similar(Δt)
-x[1]    = 0
-y[1]    = 0
-φ[1]    = 0
-Δt[1]   = 0
+Δt  = dt*(1:n_trayectorias)
+collect(Δt)
+msd = similar(x)
+x[1, :] .= 0.0
+y[1, :] .= 0.0
+φ[1, :] .= 0.0
+
 
 for j in 1:n_particulas
     for i in 1:n_trayectorias-1
-            x[i+1] = x[i] .+ v*cos(φ[i])*dt .+ sqrt(2*Dt*Dr*dt)*randn()
-            y[i+1] = y[i] .+ v*sin(φ[i])*dt .+ sqrt(2*Dt*Dr*dt)*randn()
-            φ[i+1] = φ[i] .+ Ω*dt .+ sqrt(2*Dt*Dr*dt)*randn()
-            Δt[i+1]  =+ Δt[i] .+ dt
-            msd[i+1] =(x[i] .- x[1]).^2 + (y[i] .- y[1]).^2
+        x[i+1, j] = x[i, j] + v[i] * cos(φ[i, j]) * dt + sqrt.(2 * Dt[i] * dt) .* randn()
+        y[i+1, j] = y[i, j] + v[i] * sin(φ[i, j]) * dt + sqrt.(2 * Dt[i] * dt) .* randn()
+        φ[i+1, j] = φ[i, j] + Ω[i] * dt + sqrt.(2 * Dr[i] * dt) .* randn()
     end
-    plot(Δt,msd, yaxis=log)
+    msd[:, j] = (x[:, j] .- x[1, j]).^2 + (y[:, j] .- y[1, j]).^2
+    plot(Δt, msd[:, j], yaxis = log)
     savefig("msd$j.svg")
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
