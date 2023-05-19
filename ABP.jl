3508301aedb37dbadfd696a974175878f54f09fa
Mayron González <mgonzalezv2017@udec.cl>
2023-05-19 18:18:03 -0400

    Se cambiaron los ruidos a ruidoDtx y ruidoDty respectivamente, para que sean dos procesos ruidosos, no correlacionados.

--- ABP.jl
+++ ABP.jl
@@ -1,9 +1,9 @@
-using Random, Serialization, Plots, DelimitedFiles
+using Random, Plots, DelimitedFiles
 
 folder_path = "/home/mayron/ABP"
 
 #aca se define el numero de componentes en la direccion x e y
-n_trayectorias = 100000 #se bajo el numero de pasos temporales, debido a que con muchos colapsaba el grafico
+n_trayectorias = 10000 #se bajo el numero de pasos temporales, debido a que con muchos colapsaba el grafico
 n_particulas = 100
 
 L = 200  #diametro del circulo
@@ -11,10 +11,10 @@ centro_x = 0  # centro del circulo en x
 centro_y = 0  # centro del circulo en y
 radio = L/2  # radio del circulo
 inicio_gap = 0  # inicio del agujero en la  barrera circular (en radianes)
-fin_gap = pi/10 # fin del agujero en la barrera circular (en radianes)
+fin_gap = 0 # fin del agujero en la barrera circular (en radianes)
 
-v  = 5.5    #velocidad activa de la particula
-Dt = 0.08   #Difusion Traslacional
+v  = 0    #velocidad activa de la particula
+Dt = 0.22   #Difusion Traslacional
 Dr = 0.16   #Difusion Rotacional
 Ω  = 0.0    #Constante de quiralidad   
 dt = 10e-3  #Paso temporal
@@ -31,31 +31,38 @@ sqrtT = sqrt(2*Dr*dt) #esto corresponde a √(2*Dr*dt)
         y[1]    = 0
         φ[1]    = 0
         for i in 1:n_trayectorias-1
-            ruidoDt  = sqrtD*randn() 
+            
+            ruidoDtx  = sqrtD*randn()
+            
+            ruidoDty  = sqrtD*randn() #Se definen ruidos diferentes para X e Y
+            
             ruidoDr  = sqrtT*randn()
-            x[i+1] = x[i] .+ v*cos(φ[i])*dt .+ ruidoDt
-            y[i+1] = y[i] .+ v*sin(φ[i])*dt .+  ruidoDt
+            
             φ[i+1] = φ[i] .+ Ω*dt .+  ruidoDr
+            
+            x[i+1] = x[i] .+ v*cos(φ[i])*dt .+ ruidoDtx
+            
+            y[i+1] = y[i] .+ v*sin(φ[i])*dt .+  ruidoDty
 
             # Angulo de la particula con respecto al centro del circulo
             dx = x[i+1] - centro_x
             dy = y[i+1] - centro_y
             angulo = atan(dy, dx)
 
-            # Se ajusta el angulo entre -pi y pi
+             Se ajusta el angulo entre -pi y pi
             angulo = angulo % (2*pi)
             if angulo > pi
                 angulo -= 2*pi
             end
 
-            # ver si la particula esta dentro de la zona del gap
+             ver si la particula esta dentro de la zona del gap
             if angulo >= inicio_gap && angulo <= fin_gap
                 continue  # Skip reflection if the particle is within the gap region
             end
                 # Mientras la particula este dentro del circulo y fuera del angulo del gap
             distancia = sqrt(dx^2 + dy^2)
                 
-            if distancia > radio
+            if distancia >= radio
                     # Reflexion de la particula en la barrera
                     normal_x = dx / distancia  # componente x del vector normal
                     normal_y = dy / distancia  # componente y del vector normal
@@ -77,19 +84,6 @@ sqrtT = sqrt(2*Dr*dt) #esto corresponde a √(2*Dr*dt)
     #Guarda la posicion en "traj$j_dat" y guarda el msd en "msd$j_dat" de cada particula con j el numero de la particula 
     file_path = joinpath(folder_path, "traj_$j.dat")
     writedlm(file_path, [x y])
-    # rellenar la zona exterior a la barrera, por contraste
-    #theta = LinRange(0, 2π, 100)
-    #circ_x = centro_x .+ radio * cos.(theta)
-    #circ_y = centro_y .+ radio * sin.(theta)
-    #plot(circ_x, circ_y, fillrange = 0, fillalpha = 0.3, color = :gray, aspect_ratio = :equal, legend =false)
-    #Borrar una parte de relleno para identificar visualmente el gap
-    #theta = LinRange(inicio_gap, fin_gap, 100)
-    #gap_x = centro_x .+ radio * cos.(theta)
-    #gap_y = centro_y .+ radio * sin.(theta)
-    #plot(gap_x, gap_y, color = :red, aspect_ratio = :equal, linewidth= 15, legend =false)
-    #ploteo de la trayectoria de la particula
-    #scatter(x,y, markersize=0.2, legend=false)
-    #savefig("traj_$j.svg")
 end
 
 
--- Post_Procesado.jl
+++ Post_Procesado.jl
@@ -12,7 +12,7 @@ centro_x = 0
 centro_y = 0
 radio = L/2
 inicio_gap = 0
-fin_gap = pi/10
+fin_gap = 0
 
 # Define the fill range for the circular barrier
 theta_fill = LinRange(0, 2π, 100)
@@ -42,7 +42,7 @@ for j in 1:n_particulas
         plot(circ_x_fill, circ_y_fill, fillrange = 0, fillalpha = 0.3, color = :gray, aspect_ratio = :equal, legend = false)
 
         # Plot the gap in the circular barrier
-        plot!(gap_x, gap_y, color = :white, aspect_ratio = :equal, linewidth = 15, legend = false)
+       # plot!(gap_x, gap_y, color = :white, aspect_ratio = :equal, linewidth = 15, legend = false)
 
         # Plot the particle trajectory
         scatter!(x, y, markersize = 0.2, legend = false)
