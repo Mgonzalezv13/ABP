40a9902d937e7bd23204098a07c14ef507bf2a25
Mayron González <mgonzalezv2017@udec.cl>
2023-05-31 13:02:15 -0400

    Cambios menores a Post_Procesado, se elimino el ploteo y se quito la linea que calcula el array de \Delta t

--- Post_Procesado.jl
+++ Post_Procesado.jl
@@ -8,7 +8,6 @@ dt = 10e-3
 function msd_prom(v,n_pasos,n_particulas)
     
     msd_sum = zeros(n_pasos,n_particulas)
-    Δt = range(dt, length=n_pasos, step=dt)
             
 
     @showprogress "Calculando..." for i in 1:n_particulas
@@ -18,7 +17,5 @@ function msd_prom(v,n_pasos,n_particulas)
 
     msd_promedio = mean(msd_sum, dims=2)
     writedlm("/home/mayron/Datos/datos_$v/msd_promedio$v.csv", msd_promedio, ',')
-
-    plot(Δt, msd_promedio, xaxis=:log, yaxis=:log, xlim=(0.01,1000), ylim=(0.01,10000), size=(2000,8000))
         
 end
