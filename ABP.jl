186176258e449d1a11c6d81a2b5c45192c8cc9bc
Mayron González <mgonzalezv2017@udec.cl>
2023-10-16 14:44:53 -0300

    Se definio una función, para encontrar una condicion inicial donde se evite el overlap.

--- ABP.jl
+++ ABP.jl
@@ -3,7 +3,7 @@ using Random, DelimitedFiles, ProgressMeter, Distributions
 folder_path = "/home/mayron/ABP"
 
 #aca se define el numero de componentes en la direccion x e y
-L = 50  #diametro del circulo
+L = 200  #diametro del circulo
 radio_c = L/2
 centro_x = 0  # centro del circulo en x
 centro_y = 0  # centro del circulo en y
@@ -17,10 +17,7 @@ sqrtD = sqrt(2*Dt*dt) #esto corresponde a √(2*Dt*dt)
 sqrtT = sqrt(2*Dr*dt) #esto corresponde a √(2*Dr*dt)
 uniform_dist = Uniform(0, 2π)
 
-function barrera(v, n_pasos, n_particulas, radio, Ω=0)
-    #pos_x = fill(NaN,n_pasos,n_particulas)
-    #pos_y = similar(pos_x)
-   # @showprogress "Calculando trayectorias " for j in 1:n_particulas
+function barrera(v::Int64, n_pasos::Int64, n_particulas::Int64, radio, Ω=0)
         #Aca se definen vectores "vacios" para almacenar las posiciones en x e y de cada particula 
             x   = fill(NaN,n_pasos,n_particulas)
             y   = similar(x)
@@ -30,15 +27,14 @@ function barrera(v, n_pasos, n_particulas, radio, Ω=0)
             φ[1,:] = 2pi * randn(n_particulas)
             random = sqrt(rand())
             rand_ang = randn(n_particulas)*2pi
-            x[1,:] = radio*sqrt.(rand(n_particulas)).*cos.(rand_ang)
-            y[1,:] = radio*sqrt.(rand(n_particulas)).*sin.(rand_ang)
+            x[1,:], y[1,:] = condicion_inicial(n_particulas,radio,radio_c) 
             @showprogress "Calculando trayectorias " for i in 2:n_pasos
                 
 
 
                 f_x, f_y = correccion_lj(x[i-1,:], y[i-1,:], radio)
-                forces1[i,:] = f_x
-                forces2[i,:] = f_y 
+                forces1[i-1,:] = f_x
+                forces2[i-1,:] = f_y 
                 x[i-1,:] += f_x*dt
                 y[i-1,:] += f_y*dt
 
@@ -54,15 +50,15 @@ function barrera(v, n_pasos, n_particulas, radio, Ω=0)
                 
                 y[i,:] = y[i-1,:] + v*sin.(φ[i-1,:])*dt +  ruidoDty
 
-                dx = x[i,:] .- centro_x
-                dy = y[i,:] .- centro_y
+                dx = x[i,:]  .- centro_x
+                dy = y[i,:]  .- centro_y
                 
 
-            #Verificar la posicion de la particula con respecto al centro del circulo
+                #Verificar la posicion de la particula con respecto al centro del circulo
                 distancia_c = sqrt.((dx).^2 + (dy).^2)
 
                 # Se define un array con el indice de todas las particulas que están fuera de la barrera en el i-esimo paso temporal
-                fuera = distancia_c .>= radio
+                fuera = distancia_c .>= radio_c
 
                 # Reflect particles outside the barrier
                 for j in findall(fuera)
@@ -105,10 +101,10 @@ function correccion_lj(posicion_x, posicion_y, radio)
                 r = sqrt(dx^2 + dy^2)  # Distancia entre la i-esima y j-esima particula
                 if r <= 2*radio * 2^(1/6)
                     #Potencial de interaccion de disco solido
-                        force_magnitude = lj_force(r,100,2*radio)
+                        magnitud_fuerza = lj_fuerza(r,10,2*radio)
                     #Calculamos la componente x e y de la fuerza    
-                        f_x = force_magnitude * dx 
-                        f_y = force_magnitude * dy
+                        f_x = magnitud_fuerza * dx 
+                        f_y = magnitud_fuerza * dy
                     # Updateamos el array x e y de las fuerzas
                         fuerza_x[i] += f_x
                         fuerza_y[i] += f_y
@@ -120,24 +116,47 @@ function correccion_lj(posicion_x, posicion_y, radio)
     return fuerza_x, fuerza_y
 end
 
-function lj_force(distance, epsilon, sigma)
+function lj_fuerza(distancia, epsilon, sigma)
     # Calculate the Lennard-Jones potential energy between two particles
-    r_sigma = sigma / distance
+    r_sigma = sigma / distancia
 
     # Calculate the potential energy
-    force = 24 * epsilon * (  ( (sigma^6)/(distance.^8) ) -2*( (sigma^12)/(distance.^14) )  )
+    force = 24 * epsilon * (  ( (sigma^6)/(distancia.^8) ) -2*( (sigma^12)/(distancia.^14) )  )
 
     return force
 end
 
-function lj_potential(distance, epsilon, sigma)
-    # Calculate the Lennard-Jones potential energy between two particles
-    r_sigma = sigma / distance
 
-    # Calculate the potential energy
-    potential = 4 * epsilon * ((r_sigma.^12) - (r_sigma.^7))
+function condicion_inicial(n_particulas, radio_particula, radio_circulo)
+    x_ini = Float64[]  # Array to store x-coordinates
+    y_ini = Float64[]  # Array to store y-coordinates
 
-    return potential
+    for _ in 1:n_particulas
+        while true
+            # Generar condiciones iniciales en coordenadas polares
+            angulo = rand() * 2 * π
+            r = rand() * (radio_circulo - radio_particula) + radio_particula
+
+            # Convertir a coordenas cartesianas
+            x = r * cos(angulo)
+            y = r * sin(angulo)
+
+            # Checkear si la posicion de la i-esima particula no se solapa con otra
+            overlap = false
+            for i in 1:length(x_ini)
+                if sqrt((x - x_ini[i])^2 + (y - y_ini[i])^2) < 2 * radio_particula
+                    overlapping = true
+                    break
+                end
+            end
+            #Si no hay overlap, entonces guarda la posicion en x e y
+            if !overlap
+                push!(x_ini, x)
+                push!(y_ini, y)
+                break
+            end
+        end
+    end
+
+    return x_ini, y_ini
 end
-
-#x, y = barrera(1,1000,1,1)
