39e0f399d4e8bd9588439ab7935d90563c1f7d6e
Mayron González <mgonzalezv2017@udec.cl>
2024-01-05 13:59:23 -0300

    Cambios menores

--- ABP.jl
+++ ABP.jl
@@ -3,28 +3,29 @@ using Random, DelimitedFiles, ProgressMeter, Distributions
 folder_path = "/home/mayron/ABP"
 
 #aca se define el numero de componentes en la direccion x e y
-L = 200  #diametro del circulo
+L = 50  #diametro del circulo
 radio_c = L/2
 centro_x = 0  # centro del circulo en x
 centro_y = 0  # centro del circulo en y
-inicio_gap = 0
-fin_gap =   pi/6
+#inicio_gap = 0
+#fin_gap =   pi/6
 Dt = 0   #Difusion Traslacional
 Dr = 0.3   #Difusion Rotacional
 Ω  = 0.0    #Constante de quiralidad   
-dt = 10^-3  #Paso temporal
+dt = 10^-4  #Paso temporal
 sqrtD = sqrt(2*Dt*dt) #esto corresponde a √(2*Dt*dt)
 sqrtT = sqrt(2*Dr*dt) #esto corresponde a √(2*Dr*dt)
 uniform_dist = Uniform(0, 2π)
 
 function barrera(v::Int64, n_pasos::Int64, n_particulas::Int64, radio, Ω=0)
         #Aca se definen vectores "vacios" para almacenar las posiciones en x e y de cada particula 
-            x   = fill(NaN,n_pasos,n_particulas)
+            Ro = 4*radio
+            x   = zeros(n_pasos,n_particulas)
             y   = similar(x)
             φ   = similar(x)
             forces1 = zeros(n_pasos,n_particulas)
             forces2 = zeros(n_pasos,n_particulas)
-            φ[1,:] = 2pi * randn(n_particulas)
+            φ[1,:] = rand(-pi:pi,n_particulas)
             random = sqrt(rand())
             rand_ang = randn(n_particulas)*2pi
             x[1,:], y[1,:] = condicion_inicial(n_particulas,radio,radio_c) 
@@ -33,8 +34,8 @@ function barrera(v::Int64, n_pasos::Int64, n_particulas::Int64, radio, Ω=0)
 
 
                 f_x, f_y = correccion_lj(x[i-1,:], y[i-1,:], radio)
-                forces1[i-1,:] = f_x
-                forces2[i-1,:] = f_y 
+                #forces1[i-1,:] = f_x
+                #forces2[i-1,:] = f_y 
                 x[i-1,:] += f_x*dt
                 y[i-1,:] += f_y*dt
 
@@ -54,35 +55,39 @@ function barrera(v::Int64, n_pasos::Int64, n_particulas::Int64, radio, Ω=0)
                 dy = y[i,:]  .- centro_y
                 
 
-                #Verificar la posicion de la particula con respecto al centro del circulo
+                # Verificar la posición de la partícula con respecto al centro del círculo
                 distancia_c = sqrt.((dx).^2 + (dy).^2)
 
-                # Se define un array con el indice de todas las particulas que están fuera de la barrera en el i-esimo paso temporal
-                fuera = distancia_c .>= radio_c
+                # Se define un array con el índice de todas las partículas que están fuera de la barrera en el i-ésimo paso temporal
+                fuera = distancia_c .>= (radio_c + radio)  # Tomando en cuenta el radio de la particula
 
-                # Reflect particles outside the barrier
+                # Reflejar particulas fuera de la barrera
                 for j in findall(fuera)
-                    #se calcula el vector normal al desplazamiento en x e y
+                    
+                    # Calcular el vector normal al desplazamiento en x e y
                     normal_x = dx[j] / distancia_c[j]
                     normal_y = dy[j] / distancia_c[j]
 
-                    # Reflejar la posicion de la particula
-                    reflejo_x = centro_x + normal_x * radio_c
-                    reflejo_y = centro_y + normal_y * radio_c
+                    # Calcular el punto de reflexión tomando en cuenta el radio de la particula
+                    reflejo_x = centro_x + normal_x * (radio_c + radio)
+                    reflejo_y = centro_y + normal_y * (radio_c + radio)
                     x[i, j] = reflejo_x
                     y[i, j] = reflejo_y
 
-                    # Refejar el angulo de la particula
+                    #Calcular nuevo angulo de orientacion
                     angulo = atan(dy[j], dx[j])
-                    φ[i, j] = angulo + pi / 4
+                    φ[i, j] = angulo + pi / 2
                 end
+
+
             end
                 
 
     writedlm("/home/mayron/Datos/barrera_$v/pos_x_v=00$v.csv",x , ',')
     writedlm("/home/mayron/Datos/barrera_$v/pos_y_v=00$v.csv",y , ',')
-    writedlm("/home/mayron/Datos/barrera_$v/f_x_v=00$v.csv",forces1 , ',')
-    writedlm("/home/mayron/Datos/barrera_$v/f_y_v=00$v.csv",forces2 , ',')
+    writedlm("/home/mayron/Datos/barrera_$v/theta_v=00$v.csv",φ , ',')
+    #writedlm("/home/mayron/Datos/barrera_$v/f_x_v=00$v.csv",forces1 , ',')
+    #writedlm("/home/mayron/Datos/barrera_$v/f_y_v=00$v.csv",forces2 , ',')
     return x, y
 end
 
@@ -99,57 +104,58 @@ function correccion_lj(posicion_x, posicion_y, radio)
                 dx = posicion_x[j] - posicion_x[i]
                 dy = posicion_y[j] - posicion_y[i]
                 r = sqrt(dx^2 + dy^2)  # Distancia entre la i-esima y j-esima particula
-                if r <= 2*radio * 2^(1/6)
-                    #Potencial de interaccion de disco solido
-                        magnitud_fuerza = lj_fuerza(r,10,2*radio)
-                    #Calculamos la componente x e y de la fuerza    
-                        f_x = magnitud_fuerza * dx 
-                        f_y = magnitud_fuerza * dy
+
+                if r <= 2*radio * 2^(1/6) 
+                    # Potencial de interaccion
+                    magnitud_fuerza = lj_fuerza(r, 0.5, 2 * radio)
+                    # Calculamos la componente x e y de la fuerza    
+                    f_x = magnitud_fuerza * dx 
+                    f_y = magnitud_fuerza * dy 
                     # Updateamos el array x e y de las fuerzas
-                        fuerza_x[i] += f_x
-                        fuerza_y[i] += f_y
+                    fuerza_x[i] += f_x
+                    fuerza_y[i] += f_y
                 end
             end
         end
     end
-    
     return fuerza_x, fuerza_y
 end
 
 function lj_fuerza(distancia, epsilon, sigma)
-    # Calculate the Lennard-Jones potential energy between two particles
-    r_sigma = sigma / distancia
-
-    # Calculate the potential energy
-    force = 24 * epsilon * (  ( (sigma^6)/(distancia.^8) ) -2*( (sigma^12)/(distancia.^14) )  )
-
-    return force
+        # Calculate the potential energy
+        force = 24 * epsilon * ((sigma^6) / (distancia^8) - 2 * (sigma^12) / (distancia^14))
+        return force
 end
 
 
-function condicion_inicial(n_particulas, radio_particula, radio_circulo)
+function condicion_inicial(n_particulas, radio_particula, radio_circulo, max_attempts = 100)
     x_ini = Float64[]  # Array to store x-coordinates
     y_ini = Float64[]  # Array to store y-coordinates
 
+
+
     for _ in 1:n_particulas
         while true
+            # Check if the number of attempts exceeds the specified limit
+
             # Generar condiciones iniciales en coordenadas polares
             angulo = rand() * 2 * π
             r = rand() * (radio_circulo - radio_particula) + radio_particula
 
-            # Convertir a coordenas cartesianas
+            # Convertir a coordenadas cartesianas
             x = r * cos(angulo)
             y = r * sin(angulo)
 
-            # Checkear si la posicion de la i-esima particula no se solapa con otra
+            # Chequear si la posición de la i-ésima partícula no se solapa con otra
             overlap = false
             for i in 1:length(x_ini)
                 if sqrt((x - x_ini[i])^2 + (y - y_ini[i])^2) < 2 * radio_particula
-                    overlapping = true
+                    overlap = true
                     break
                 end
             end
-            #Si no hay overlap, entonces guarda la posicion en x e y
+
+            # Si no hay overlap, entonces guarda la posición en x e y
             if !overlap
                 push!(x_ini, x)
                 push!(y_ini, y)
