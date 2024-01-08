ad7cf1570957fd44f36c8f34a287a2f65acd5bb9
Mayron González <mgonzalezv2017@udec.cl>
2024-01-08 12:15:20 -0300

    Se definieron 2 nuevas funciones, una que construye una barrera compuesta por particulas inamovibles y la otra funcion se encarga de ver las interacciones entre las Abps y la barrera.

--- ABP.jl
+++ ABP.jl
@@ -29,15 +29,20 @@ function barrera(v::Int64, n_pasos::Int64, n_particulas::Int64, radio, Ω=0)
             random = sqrt(rand())
             rand_ang = randn(n_particulas)*2pi
             x[1,:], y[1,:] = condicion_inicial(n_particulas,radio,radio_c) 
+            barrera_x, barrera_y = generar_barrera(0,0,25,radio)
             @showprogress "Calculando trayectorias " for i in 2:n_pasos
                 
 
 
                 f_x, f_y = correccion_lj(x[i-1,:], y[i-1,:], radio)
+                fx, fy = chequear_barrera(x[i-1,:], y[i-1,:],barrera_x,barrera_y,radio)
                 #forces1[i-1,:] = f_x
                 #forces2[i-1,:] = f_y 
                 x[i-1,:] += f_x*dt
                 y[i-1,:] += f_y*dt
+                x[i-1,:] += fx*dt
+                y[i-1,:] += fy*dt
+
 
                 ruidoDtx = sqrtD * randn(n_particulas)
                 
@@ -56,28 +61,28 @@ function barrera(v::Int64, n_pasos::Int64, n_particulas::Int64, radio, Ω=0)
                 
 
                 # Verificar la posición de la partícula con respecto al centro del círculo
-                distancia_c = sqrt.((dx).^2 + (dy).^2)
+                #distancia_c = sqrt.((dx).^2 + (dy).^2)
 
                 # Se define un array con el índice de todas las partículas que están fuera de la barrera en el i-ésimo paso temporal
-                fuera = distancia_c .>= (radio_c + radio)  # Tomando en cuenta el radio de la particula
+                #fuera = distancia_c .>= (radio_c + radio)  # Tomando en cuenta el radio de la particula
 
                 # Reflejar particulas fuera de la barrera
-                for j in findall(fuera)
+                #for j in findall(fuera)
                     
                     # Calcular el vector normal al desplazamiento en x e y
-                    normal_x = dx[j] / distancia_c[j]
-                    normal_y = dy[j] / distancia_c[j]
+                 #   normal_x = dx[j] / distancia_c[j]
+                 #   normal_y = dy[j] / distancia_c[j]
 
                     # Calcular el punto de reflexión tomando en cuenta el radio de la particula
-                    reflejo_x = centro_x + normal_x * (radio_c + radio)
-                    reflejo_y = centro_y + normal_y * (radio_c + radio)
-                    x[i, j] = reflejo_x
-                    y[i, j] = reflejo_y
+                 #   reflejo_x = centro_x + normal_x * (radio_c + radio)
+                 #   reflejo_y = centro_y + normal_y * (radio_c + radio)
+                 #   x[i, j] = reflejo_x
+                 #   y[i, j] = reflejo_y
 
                     #Calcular nuevo angulo de orientacion
-                    angulo = atan(dy[j], dx[j])
-                    φ[i, j] = angulo + pi / 2
-                end
+                 #   angulo = atan(dy[j], dx[j])
+                 #   φ[i, j] = angulo + pi / 2
+                #end
 
 
             end
@@ -86,6 +91,8 @@ function barrera(v::Int64, n_pasos::Int64, n_particulas::Int64, radio, Ω=0)
     writedlm("/home/mayron/Datos/barrera_$v/pos_x_v=00$v.csv",x , ',')
     writedlm("/home/mayron/Datos/barrera_$v/pos_y_v=00$v.csv",y , ',')
     writedlm("/home/mayron/Datos/barrera_$v/theta_v=00$v.csv",φ , ',')
+    writedlm("/home/mayron/Datos/barrera_$v/barrera_x.csv",φ , ',')
+    writedlm("/home/mayron/Datos/barrera_$v/barrera_y.csv",φ , ',')
     #writedlm("/home/mayron/Datos/barrera_$v/f_x_v=00$v.csv",forces1 , ',')
     #writedlm("/home/mayron/Datos/barrera_$v/f_y_v=00$v.csv",forces2 , ',')
     return x, y
@@ -140,7 +147,7 @@ function condicion_inicial(n_particulas, radio_particula, radio_circulo, max_att
 
             # Generar condiciones iniciales en coordenadas polares
             angulo = rand() * 2 * π
-            r = rand() * (radio_circulo - radio_particula) + radio_particula
+            r = rand(0:(radio_circulo - radio_particula)) 
 
             # Convertir a coordenadas cartesianas
             x = r * cos(angulo)
@@ -196,4 +203,43 @@ function quorum_sensing(posicion_x, posicion_y, radio,φ,vc,Ro)
         end
     end
     return 
+end
+
+
+function generar_barrera(centro_x, centro_y, radio_c, radio)
+    circunferencia = 2 * π * radio_c
+    num_particles = round(Int, circunferencia / (2 * radio))
+
+    θ = range(0, stop=2π, length=num_particles+1)[1:end-1]  # Angular positions for particles
+    x = centro_x .+ (radio_c - radio) * cos.(θ)
+    y = centro_y .+ (radio_c - radio) * sin.(θ)
+
+    return x, y
+end
+
+
+function chequear_barrera(posicion_x, posicion_y, barrera_x, barrera_y, radio)
+    n_particulas = length(posicion_x)
+    fuerza_x = zeros(n_particulas)
+    fuerza_y = zeros(n_particulas)
+    
+    for i in 1:n_particulas
+        for j in 1:length(barrera_x)
+            dx = barrera_x[j] - posicion_x[i]
+            dy = barrera_y[j] - posicion_y[i]
+            r = sqrt(dx^2 + dy^2)  # Distancia entre las particulas y la barrera
+
+            if r <= 2 * radio
+                # Potencial de interacción
+                magnitud_fuerza = lj_fuerza(r, 0.5, 2 * radio)
+                # Calculamos la componente x e y de la fuerza    
+                f_x = magnitud_fuerza * dx  
+                f_y = magnitud_fuerza * dy  
+                # Actualizamos el array x e y de las fuerzas
+                fuerza_x[i] += f_x
+                fuerza_y[i] += f_y
+            end
+        end
+    end
+    return fuerza_x, fuerza_y
 end
