d12f29ead0be4b77be12b02cd534a8f5a7bc3669
Mayron.V <mayron.agv@gmail.com>
2025-12-20 21:43:42 -0300

    Se cambio la definicion de la barrera y como interactua la barrera con los abps

--- ABP.jl
+++ ABP.jl
@@ -1,112 +1,156 @@
-using Random, DelimitedFiles, ProgressMeter, Distributions, LinearAlgebra
-
-folder_path = "/home/mayron/ABP"
-
-#aca se define el numero de componentes en la direccion x e y
-L = 500  #diametro del circulo
-Dt = 0   #Difusion Traslacional
-Dr = 0.01   #Difusion Rotacional
-Ω  = 0.0    #Constante de quiralidad   
-dt = 10^-3  #Paso temporal
-sqrtD = sqrt(2*Dt*dt) #esto corresponde a √(2*Dt*dt)
-sqrtT = sqrt(2*Dr*dt) #esto corresponde a √(2*Dr*dt)
+using  DelimitedFiles, LinearAlgebra, Printf, Dates, Clustering, Statistics, ProgressMeter, Random, Distances
 
 
 
-function vc(v::Int64, n_pasos::Int64, n_particulas::Int64, radio,angulo1::Float64)
+dt = 1e-3  #Paso temporal
+
+
+
+
+function vc(v::Int64, n_pasos::Int64, n_particulas::Int64, radio::Int64, angulo1::Float64,α,Dr,radio_p = 1, radio_b = 0.1)
+   
+     # Carpeta donde se guardara los datos de la simulacion
+     carpeta = carpeta_simulacion("/home/mayron/Datos", angulo1,n_particulas,Dr,α)
+        η       = packing_fraction(n_particulas,radio)
+
+     # Archivo log con los parámetros de la simulacion
+        generar_log(carpeta, v, n_pasos, n_particulas, radio, angulo1,η,α,Dr,dt)
+     # Guardar seed
+        #Random.seed!(seed)
+   
+        sqrtT = sqrt(2*Dr*dt)
     #Aca se definen vectores "vacios" para almacenar las posiciones en x e y de cada particula 
-        x   = zeros(n_pasos,n_particulas)
-        y   = similar(x)
-        φ   = similar(x)
-        quorum1 = zeros(n_pasos,n_particulas)
-        m = zeros(n_pasos) #magnetizacion
-        φ[1,:] = rand(0:2pi,n_particulas).*randn(n_particulas)
-        x[1,:] , y[1,:] = condicion_inicial(n_particulas,radio,48)
-        m[1] = abs.((sum( ( cos.( φ[1,:] ) ) + ( sin.( φ[1,:] ) ) ))/n_particulas)
-        barrera_x, barrera_y = generar_barrera(0,0,50,radio)
-        @showprogress "Calculando trayectorias " for i in 2:n_pasos
+        x_data   = Matrix{Float64}(undef, Int64(n_pasos/100), n_particulas)
+        y_data   = Matrix{Float64}(undef, Int64(n_pasos/100), n_particulas)
+        vx_data  = Matrix{Float64}(undef, Int64(n_pasos/100), n_particulas)
+        vy_data  = Matrix{Float64}(undef, Int64(n_pasos/100), n_particulas)
+        φ_data   = Matrix{Float64}(undef, Int64(n_pasos/100), n_particulas)
+        φ_old = rand(0:2pi,n_particulas)
+        x_old , y_old = condicion_inicial(n_particulas,radio)
+        b_x, b_y = generar_barrera(0,0,radio,radio_b)
+        vf,vc = vecinos(x_old,y_old,α)
+        vb    = vecinos_barrera(x_old,y_old,b_x,b_y)
+        @showprogress "Calculando..." for i in 2:n_pasos
+
+          
+            f_x, f_y    = correccion_lj(x_old, y_old, vf,radio_p,n_particulas)
+            f_bx, f_by  = correccion_barrera(x_old, y_old, b_x, b_y,vb, radio_p,radio_b, n_particulas)
+            quorum, Nc  = quorum_sensing(x_old, y_old, n_particulas,φ_old, angulo1,vc,α)
             
 
-            
-            f_x, f_y = correccion_lj(x[i-1,:], y[i-1,:], radio)
-            quorum, Nc = quorum_sensing(x[i-1,:],y[i-1,:],n_particulas,φ[i-1,:],angulo1)
-            τ = torque_barrera(x[i-1,:], y[i-1,:],barrera_x,barrera_y,φ[i-1,:], radio)
-            quorum1[i-1,:] = quorum
-            x[i-1,:] += f_x * dt
-            y[i-1,:] += f_y * dt
-
-
-
-
-            ruidoDtx = sqrtD * randn(n_particulas)
-            
-            ruidoDty = sqrtD * randn(n_particulas)
             
             ruidoDr  = sqrtT * randn(n_particulas)
+
+            vx = v*cos.(φ_old)  + f_x + f_bx
             
-            φ[i,:] = φ[i-1,:] + 5*(quorum./Nc)*dt   +  τ   +  ruidoDr
+            vy = v*sin.(φ_old)  + f_y + f_by
+
+            φ = φ_old + 5*(quorum./Nc)*dt + ruidoDr 
             
-            x[i,:] = x[i-1,:] + v*cos.(φ[i-1,:])*dt + ruidoDtx
+            x = x_old + vx*dt
             
-            y[i,:] = y[i-1,:] + v*sin.(φ[i-1,:])*dt +  ruidoDty
+            y = y_old + vy*dt
 
-            m[i] = abs.(sum( ( cos.( φ[i,:] ) ) + ( sin.( φ[i,:] ) ) )/(n_particulas))
+            
+            
+            if i % 100 == 0
+                # Guardar datos cada 100 pasos de tiempo
+                x_data[Int64(i/100),:]  .= x 
+                y_data[Int64(i/100),:]  .= y
+                φ_data[Int64(i/100),:]  .= φ
+                vx_data[Int64(i/100),:] .= vx
+                vy_data[Int64(i/100),:] .= vy
+            end
 
 
-            #x[i,:], y[i,:] = periodic_bc(x[i,:],y[i,:],n_particulas,L)
+
+               
+                
+                if i % 200 == 0
+
+                    #actualizar la lista de vecinos cada ciertos pasos
+                    vf,vc = vecinos(x,y,α)
+                    vb    = vecinos_barrera(x,y,b_x,b_y)
+
+                end
+                
+        
+
+                
 
 
+                φ_old = φ
+                x_old = x
+                y_old = y 
 
             end  
-
-    writedlm("/home/mayron/Datos/barrera_$v/pos_x_v=00$v.csv",x , ',')
-    writedlm("/home/mayron/Datos/barrera_$v/pos_y_v=00$v.csv",y , ',')
-    writedlm("/home/mayron/Datos/barrera_$v/theta_v=00$v.csv",φ , ',')
-    #writedlm("/home/mayron/Datos/barrera_$v/fuerza_x.csv",f1 , ',')
-    #writedlm("/home/mayron/Datos/barrera_$v/fuerza_y.csv",f2 , ',')
-    #writedlm("/home/mayron/Datos/barrera_$v/φ=$angulo1/_magnetizacion_N=$n_particulas.csv",m , ',')
+        # Guardar las posiciones en los archivos
+        writedlm(joinpath(carpeta, "pos_x_v=$(round(v, digits=2)).csv"), x_data, ',')
+        writedlm(joinpath(carpeta, "pos_y_v=$(round(v, digits=2)).csv"), y_data, ',')
+        writedlm(joinpath(carpeta, "phi_v=$(round(v, digits=2)).csv"), φ_data, ',')
+        writedlm(joinpath(carpeta, "vx=$(round(v, digits=2)).csv"), vx_data, ',')
+        writedlm(joinpath(carpeta, "vy=$(round(v, digits=2)).csv"), vy_data, ',')
         
-    #writedlm("/home/mayron/Datos/barrera_$v/quorum.csv",quorum1 , ',')
-    return x, y
+    return
 end
 
 
-function correccion_lj(posicion_x, posicion_y, radio)
-    n_particulas = length(posicion_x)
+function correccion_barrera(pos_x, pos_y, barrera_x, barrera_y,vecinos_b, radio_p,radio_bar, n_particulas)
+
+    fx = zeros(n_particulas)
+    fy = zeros(n_particulas)
+
+    for i in 1:n_particulas
+        for j in vecinos_b[i]
+
+        dx = barrera_x[j] - pos_x[i]
+        dy = barrera_y[j] - pos_y[i]
+        r = sqrt(dx^2 + dy^2)
+
+        sigma = (radio_bar + radio_p)/2
+
+            if r <= 2 * sigma
+            F = soft_fuerza(r, 125, 2 * sigma)
+
+            fx[i] -= F * (dx / r)
+            fy[i] -= F * (dy / r)
+            end
+        end
+    end
+        return fx, fy
+end
+
+function correccion_lj(posicion_x, posicion_y,vecinos, radio,n_particulas)
     fuerza_x = zeros(n_particulas)
     fuerza_y = zeros(n_particulas)
     
-    Threads.@threads for i in 1:n_particulas
-        for j in 1:n_particulas
-            if i != j
+     for i in 1:n_particulas
+        for j in vecinos[i]
                 dx = posicion_x[j] - posicion_x[i]
                 dy = posicion_y[j] - posicion_y[i]
                 r = sqrt(dx^2 + dy^2)  # Distancia entre la i-esima y j-esima particula
-
-                if r <= 2*radio * 2^(1/6) 
+                if r <= 2*radio
                     # Potencial de interaccion
-                    magnitud_fuerza = lj_fuerza(r, 0.5, 2 * radio)
+                    magnitud_fuerza = soft_fuerza(r, 75, 2 * radio)
                     # Calculamos la componente x e y de la fuerza    
-                    f_x = magnitud_fuerza * dx 
-                    f_y = magnitud_fuerza * dy 
+                    f_x = -magnitud_fuerza * (dx/r) 
+                    f_y = -magnitud_fuerza * (dy/r) 
                     # Updateamos el array x e y de las fuerzas
                     fuerza_x[i] += f_x
                     fuerza_y[i] += f_y
                 end
-            end
         end
     end
     return fuerza_x, fuerza_y
 end
 
-function lj_fuerza(distancia, epsilon, sigma)
-        # Calculate the potential energy
-        force = 24 * epsilon * ((sigma^6) / (distancia^8) - 2 * (sigma^12) / (distancia^14))
-        return force
+
+function soft_fuerza(distancia, epsilon, sigma)
+    return epsilon * (1 - (distancia /(sigma)))^(3/2)
 end
 
 
-function condicion_inicial(n_particulas, radio_particula, radio_circulo, max_attempts = 100)
+function condicion_inicial(n_particulas,radio_circulo,radio_particula = 1, max_attempts = 100)
     x_ini = Float64[]  # Array to store x-coordinates
     y_ini = Float64[]  # Array to store y-coordinates
 
@@ -114,7 +158,6 @@ function condicion_inicial(n_particulas, radio_particula, radio_circulo, max_att
 
     for _ in 1:n_particulas
         while true
-            # Check if the number of attempts exceeds the specified limit
 
             # Generar condiciones iniciales en coordenadas polares
             angulo = rand() * 2 * π
@@ -126,7 +169,7 @@ function condicion_inicial(n_particulas, radio_particula, radio_circulo, max_att
 
             # Chequear si la posición de la i-ésima partícula no se solapa con otra
             overlap = false
-            for i in 1:eachindex(x_ini)
+            for i in 1:length(x_ini)
                 if sqrt((x - x_ini[i])^2 + (y - y_ini[i])^2) < 2 * radio_particula
                     overlap = true
                     break
@@ -146,13 +189,13 @@ function condicion_inicial(n_particulas, radio_particula, radio_circulo, max_att
 end
 
 
-function quorum_sensing(posicion_x, posicion_y, n_particulas, φ, angulo1, Ro = 3)
+function quorum_sensing(posicion_x, posicion_y, n_particulas, φ, angulo1,vecinos,α,Ro=3)
     
     quorum = zeros(n_particulas)
     Nc = ones(n_particulas)
     
-    Threads.@threads for i in 1:n_particulas
-        for j in 1:n_particulas
+     for i in 1:n_particulas
+        for j in vecinos[i]
             if i != j
 
                 dx = posicion_x[j] - posicion_x[i]
@@ -163,7 +206,7 @@ function quorum_sensing(posicion_x, posicion_y, n_particulas, φ, angulo1, Ro = 
 
                 #angulo = atan(rij[2],rij[1])
 
-                if (dot(rij, [cos(φ[i]), sin(φ[i])]) >= cos(angulo1)) && (r <= 4 * Ro)
+                if (dot(rij, [cos(φ[i]), sin(φ[i])]) >= cos(angulo1)) && (r <= α * Ro)
                     # si las particulas estan dentro del cono de vision y a la distancia indicada
 
 
@@ -172,6 +215,9 @@ function quorum_sensing(posicion_x, posicion_y, n_particulas, φ, angulo1, Ro = 
                     quorum[i] += exp(-r/ Ro)*sin(angulo - φ[i])
                     Nc[i] += exp(-r/ Ro)
                 end
+
+                
+
             end
         end
     end 
@@ -179,7 +225,7 @@ function quorum_sensing(posicion_x, posicion_y, n_particulas, φ, angulo1, Ro = 
 end
 
 function periodic_bc(posicion_x, posicion_y, n_particulas, L)
-    Threads.@threads for i in 1:n_particulas
+     for i in 1:n_particulas
         if posicion_x[i] > L/2
             posicion_x[i] -= L
         end
@@ -202,6 +248,98 @@ end
 
 
 
+
+
+function carpeta_simulacion(base_dir, angulo1, n_particulas, Dr, α)
+    # Convert angle to nice π format if it's a multiple of π
+    angle_str = if angulo1 == π
+        "π"
+    elseif angulo1 == π/2
+        "π_2"
+    elseif angulo1 == π/3
+        "π_3"
+    elseif angulo1 == π/4
+        "π_4"
+    elseif angulo1 == π/10
+        "π_10"
+    else
+        "$angulo1"
+    end
+
+    
+    param_str = "Barrera_N=$(n_particulas)-θ=$(angle_str)-Dr=$(Dr)-Ro=$(3*α)"
+    nombre_carpeta = joinpath(base_dir, param_str)
+
+    
+    contador_sim = 1
+    nombre_carpeta = joinpath(base_dir, param_str)
+    
+    
+    if isdir(nombre_carpeta)
+        while isdir(joinpath(base_dir, "$param_str-repeticion_$contador_sim"))
+            contador_sim += 1
+        end
+        nombre_carpeta = joinpath(base_dir, "$param_str-repeticion_$contador_sim")
+    end
+
+    mkdir(nombre_carpeta)
+    return nombre_carpeta
+end
+
+function generar_log(folder_path, v, n_pasos, n_particulas, R, angulo1, η, α, Dr, dt)
+    # Write parameters to log file
+    log_filename = joinpath(folder_path, "log.txt")
+    open(log_filename, "w") do file
+        println(file, "Parámetros de la simulación:")
+        println(file, "Ángulo: $angulo1")
+        println(file, "N partículas: $n_particulas")
+        println(file, "Dr: $Dr")
+        println(file, "dt: $dt")
+        println(file, "v: $v")
+        println(file, "Iteraciones: $n_pasos")
+        println(file, "Radio Confinamiento: $R")
+        println(file, "Empaquetamiento: $η")
+        println(file, "Tamaño cono: $(3*α)")
+    end
+end
+
+
+function packing_fraction(N, R, r=1)
+    η = N * r^2 / R^2
+    return η
+end
+
+
+
+
+function vecinos(x,y, rq = 2)
+    
+    veci_fuerza = Vector{Int}[]
+    veci_cono = Vector{Int}[]
+    pos = hcat(x,y)
+
+
+    r = pairwise(Euclidean(), pos, dims=1)
+
+    r_fuerza = 2*2^(1/6) +1.0
+    r_quorum = rq*3 +1.0
+    for i in 1:size(r,2) 
+
+        v_fuerza = findall(0 .< r[i,:] .<= r_fuerza)
+
+        v_quorum = findall(0 .< r[i,:] .<= r_quorum)
+
+        push!(veci_fuerza, v_fuerza)
+
+        push!(veci_cono, v_quorum)
+
+    end
+
+
+    return veci_fuerza, veci_cono
+end
+
+
 function generar_barrera(centro_x, centro_y, radio_c, radio=0.5)
     circunferencia = 2 * π * radio_c
     num_particles = round(Int, circunferencia / (2 * radio))
@@ -214,26 +352,24 @@ function generar_barrera(centro_x, centro_y, radio_c, radio=0.5)
 end
 
 
-function torque_barrera(posicion_x, posicion_y, barrera_x, barrera_y, φ, radio)
-    n_particulas = length(posicion_x)
-    torque = zeros(n_particulas)
-    
-    for i in 1:n_particulas
-        Threads.@threads for j in 1:eachindex(barrera_x)
-            dx = barrera_x[j] - posicion_x[i]
-            dy = barrera_y[j] - posicion_y[i]
-            r = sqrt(dx^2 + dy^2)  # Distancia entre las particulas y la barrera
 
-            if r <= 2 * radio * 2^(1/6)
-                # Calculamos "n_wall"
-                N_wall = [barrera_x[j], barrera_y[j], 0] / norm([barrera_x[j], barrera_y[j]])
-                t_wall = cross(N_wall, [0, 0, 1])
+function vecinos_barrera(x_spp, y_spp, x_bar, y_bar, cutoff=3.0)
+    n_spp = length(x_spp)
+    n_bar = length(x_bar)
 
-                # Calculo del torque
-                torque[i] = -(dot([cos(φ[i]), sin(φ[i]), 0], N_wall)) * (dot([cos(φ[i]), sin(φ[i]), 0], t_wall))
+    vec = Vector{Vector{Int}}(undef, n_spp)
+
+    for i in 1:n_spp
+        list = Int[]
+        for j in 1:n_bar
+            dx = x_bar[j] - x_spp[i]
+            dy = y_bar[j] - y_spp[i]
+            if dx^2 + dy^2 <= cutoff^2
+                push!(list, j)
             end
         end
+        vec[i] = list
     end
-    
-    return torque
+
+    return vec
 end
